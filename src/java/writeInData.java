f5808abbd0c33190afed563be4f51cd877da161b
Sylvain Cuomo <zenith1313@gmail.com>
2017-03-31 16:09:25 +0200

    Write in data

--- src/java/writeInData.java
+++ src/java/writeInData.java
@@ -1,17 +1,14 @@
 /**
  * Created by Sylvai on 31/03/2017.
  */
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 public class writeInData {
 
-    public static void writeInfo(String date, String userName, String text) throws FileNotFoundException {
-       FileOutputStream fos = new FileOutputStream(new File("../../data.csv"));
-        final PrintStream printStream = new PrintStream(fos);
-        printStream.print('\"'  + date + "\";\"" + userName + "\";\"" + text + "\";");
-        printStream.close();
+    public static void writeInfo(String date, String userName, String text) throws IOException {
+        FileWriter fos = new FileWriter(new File("../../data.csv"), true);
+        fos.write('\"'  + date + "\";\"" + userName + "\";\"" + text + "\";");
+        fos.flush();
+        fos.close();
     }
 }
