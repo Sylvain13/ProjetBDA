892100cc5258c23efd8ec9b762ffb67257113cc6
Sylvain Cuomo <zenith1313@gmail.com>
2017-03-31 16:58:51 +0200

    readInfo marche writeInfo marche

---
+++ src/java/Calcul.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+/**
+ * Created by Sylvai on 31/03/2017.
+ */
+public class Calcul {
+    private static Calcul instance = new Calcul();
+
+    private Calcul(){}
+
+    public static Calcul getInstance(){
+        return instance;
+    }
+
+    public static HashMap<Integer, String> getMotifFreq() /*Integer = freqMotif */{
+
+        return null;
+    }
+}
