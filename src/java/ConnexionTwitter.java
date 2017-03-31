b8583dba90874f71b9404b800296034d004ab345
Sylvain Cuomo <zenith1313@gmail.com>
2017-03-31 15:09:15 +0200

    restructuration du git

---
+++ src/java/ConnexionTwitter.java
@@ -0,0 +1,68 @@
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Sylvai on 31/03/2017.
+ */
+public class ConnexionTwitter {
+    public static void main(String args[]) throws Exception{
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer("[consumer key]", "[consumer secret]");
+        RequestToken requestToken = twitter.getOAuthRequestToken();
+        AccessToken accessToken = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (null == accessToken) {
+            System.out.println("Open the following URL and grant access to your account:");
+            System.out.println(requestToken.getAuthorizationURL());
+            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+            String pin = br.readLine();
+            try{
+                if(pin.length() > 0){
+                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                }else{
+                    accessToken = twitter.getOAuthAccessToken();
+                }
+            } catch (TwitterException te) {
+                if(401 == te.getStatusCode()){
+                    System.out.println("Unable to get the access token.");
+                }else{
+                    te.printStackTrace();
+                }
+            }
+        }
+        //persist to the accessToken for future reference.
+        storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
+        Status status = twitter.updateStatus(args[0]);
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        System.exit(0);
+    }
+    private static void storeAccessToken(int useId, AccessToken accessToken){
+        //store accessToken.getToken()
+        //store accessToken.getTokenSecret()
+    }
+    //After you acquired the AccessToken for the user, the RequestToken is not required anymore. You can persist the AccessToken to any kind of persistent store such as RDBMS, or File system by serializing the object, or by geting the token and the secret from AccessToken#getToken() and AccessToken#getTokenSecret().
+    public static void main(String args[]) throws Exception{
+        // The factory instance is re-useable and thread safe.
+        TwitterFactory factory = new TwitterFactory();
+        AccessToken accessToken = loadAccessToken(Integer.parseInt(args[0]));
+        Twitter twitter = factory.getInstance();
+        twitter.setOAuthConsumerKey("[consumer key]", "[consumer secret]");
+        twitter.setOAuthAccessToken(accessToken);
+        Status status = twitter.updateStatus(args[1]);
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        System.exit(0);
+    }
+    private static AccessToken loadAccessToken(int useId){
+        String token = // load from a persistent store
+                String tokenSecret = // load from a persistent store
+        return new AccessToken(token, tokenSecret);
+    }
+}
