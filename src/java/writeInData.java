23173fdaa36c731c0291b33e328c0e6619210397
Sylvain Cuomo <zenith1313@gmail.com>
2017-03-31 16:15:29 +0200

    Write in data

--- src/java/writeInData.java
+++ src/java/writeInData.java
@@ -6,9 +6,17 @@ import java.io.*;
 public class writeInData {
 
     public static void writeInfo(String date, String userName, String text) throws IOException {
-        FileWriter fos = new FileWriter(new File("../../data.csv"), true);
+        FileWriter fos = new FileWriter(new File("data.csv"), true);
         fos.write('\"'  + date + "\";\"" + userName + "\";\"" + text + "\";");
         fos.flush();
         fos.close();
+
+        FileReader fr = new FileReader(new File("data.csv"));
+        char [] a = new char[50];
+        fr.read(a);   // reads the content to the array
+
+        for(char c : a)
+            System.out.print(c);   // prints the characters one by one
+        fr.close();
     }
 }
