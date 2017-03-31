5faa3fb6a38be6bff5bf71df61077e58874e9f74
Sylvain Cuomo <zenith1313@gmail.com>
2017-03-31 16:18:19 +0200

    WriteInData convert Date to string

--- src/java/writeInData.java
+++ src/java/writeInData.java
@@ -2,10 +2,18 @@
  * Created by Sylvai on 31/03/2017.
  */
 import java.io.*;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class writeInData {
 
-    public static void writeInfo(String date, String userName, String text) throws IOException {
+    public static void writeInfo(Date dateT, String userName, String text) throws IOException {
+
+
+        Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = formatter.format(dateT);
+
         FileWriter fos = new FileWriter(new File("data.csv"), true);
         fos.write('\"'  + date + "\";\"" + userName + "\";\"" + text + "\";");
         fos.flush();
